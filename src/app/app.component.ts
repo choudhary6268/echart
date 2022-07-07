49e9e80ff3f60e660b993753a942ab27ad99e140
Mandeep Singh <mandeep6268@icloud.com>
2022-07-08 00:08:14 +0530

    Chart Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,120 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'charts';
+  options= <EChartsOption>{
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {
+      data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        axisTick: { show: false },
+        data: ['2012', '2013', '2014', '2015', '2016']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Forest',
+        type: 'bar',
+        barGap: 0,
+        emphasis: {
+          focus: 'series'
+        },
+        data: [320, 332, 301, 334, 390]
+      },
+      {
+        name: 'Steppe',
+        type: 'bar',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 182, 191, 234, 290]
+      },
+      {
+        name: 'Desert',
+        type: 'bar',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [150, 232, 201, 154, 190]
+      },
+      {
+        name: 'Wetland',
+        type: 'bar',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [98, 77, 101, 99, 40]
+      }
+    ]
+  };
+  mode: any = null;
+  selected: any = { startDate: null, endDate: null };
+  singleDatePicker: boolean = true;
+  alwaysShowCalendars: boolean = true;
+  visible = false;
+  constructor() {
+    this.selected.startDate = moment();
+    this.selected.endDate = moment();
+  }
+
+
+
+  isInvalidDate(date: any) {
+    return date.weekday() === 0;
+  }
+  isCustomDate(date: any) {
+    return  (
+      date.weekday() === 0 ||
+      date.weekday() === 6
+    )  ? 'mycustomdate' : false;
+  }
+  showToday() {
+    this.visible = !this.visible;
+    this.mode = 'day';
+    this.singleDatePicker = true;
+    this.selected.startDate = moment();
+    this.selected.endDate =  moment();
+  }
+
+  showWeek() {
+    this.visible = !this.visible;
+    this.mode = 'week';
+    this.singleDatePicker = true;
+    this.selected.startDate = moment().subtract(7, 'days').startOf('day');
+    this.selected.endDate = moment();
+  }
+
+  showMonth() {
+    this.visible = !this.visible;
+    this.mode = 'month';
+    this.singleDatePicker = true;
+    this.selected.startDate = moment().startOf('month');
+    this.selected.endDate = moment().endOf('month');
+  }
+
+
+  showCustom() {
+    this.visible = !this.visible;
+    this.mode = 'custom';
+    this.singleDatePicker = false;
+    this.selected.startDate = moment();
+    this.selected.endDate =  moment();
+  }
+
+  choosedDate(selectedDate: any) {
+    this.selected.startDate = selectedDate.startDate;
+    this.selected.endDate =  selectedDate.endDate;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { NgxDaterangepickerMd as NgxDaterangepickerModule } from 'ngx-daterangepicker-material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonToggleModule,
+    NgxDaterangepickerModule.forRoot(),
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
